e1c3ce322a9eb870699761d7429af30ad4e6dbbf
basilosauridae <74819688+basilosauridae@users.noreply.github.com>
2022-09-19 07:36:57 +0000

    Deploying to gh-pages from  @ 67713efd8513a3ad350c7e889bdf793fec46aae3 🚀

---
+++ assets/vue3.html.1d3775bb.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c,a as o}from"./app.2af5f421.js";const _={},n=o("p",null,"second page in Vue",-1),s=[n];function a(r,l){return t(),c("div",null,s)}var d=e(_,[["render",a],["__file","vue3.html.vue"]]);export{d as default};
