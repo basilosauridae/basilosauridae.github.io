e4f6b5e291326820b2cb2f332ef3bcc6a3c6a114
basilosauridae <2369700690@qq.com>
2026-08-06 23:00:51 +0800

    mode

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -11,7 +11,7 @@ export default defineUserConfig({
     author: "Ying",
     authorAvatar: "/avatar.png",
     primaryColor: 'mediumpurple',
-    colorMode: 'dark',
+    colorMode: 'dark',//dark、light、auto
     docsRepo: "https://github.com/basilosauridae",
     docsBranch: "main",
     docsDir: '/blogs',
